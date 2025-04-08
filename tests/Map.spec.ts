09b074b6568a930ae2ebdaf6f0e19eeb018a0e2f
zzzzzzzzzzb <axlchao@foxmail.com>
2025-04-08 09:07:47 +0800

    fix delete map bug

--- tests/Map.spec.ts
+++ tests/Map.spec.ts
@@ -90,15 +90,15 @@ describe('Map', () => {
     });
 
     it.only('should clear old values', async () => {
-        // await map.sendClear(deployer.getSender(), toNano('0.05'), {
-        //     queryId: 123n,
-        // });
+        await map.sendClear(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+        });
     
-        // let [validUntil, value] = await map.getKey(1n);
-        // expect(validUntil).toEqual(1000n);
-        // expect(value).toEqualSlice(
-        //     beginCell().storeUint(123, 16).endCell().asSlice()
-        // );
+        let [validUntil, value] = await map.getKey(1n);
+        expect(validUntil).toEqual(1000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123, 16).endCell().asSlice()
+        );
     
         blockchain.now = 1500;
     
@@ -106,29 +106,27 @@ describe('Map', () => {
             queryId: 123n,
         });
 
-        // [validUntil, value] = await map.getKey(1n);
-
         await expect(map.getKey(1n)).rejects.toThrow();
     
-        // [validUntil, value] = await map.getKey(2n);
-        // expect(validUntil).toEqual(2000n);
-        // expect(value).toEqualSlice(
-        //     beginCell().storeUint(234, 16).endCell().asSlice()
-        // );
+        [validUntil, value] = await map.getKey(2n);
+        expect(validUntil).toEqual(2000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(234, 16).endCell().asSlice()
+        );
     
-        // [validUntil, value] = await map.getKey(3n);
-        // expect(validUntil).toEqual(3000n);
-        // expect(value).toEqualSlice(
-        //     beginCell().storeUint(345, 16).endCell().asSlice()
-        // );
+        [validUntil, value] = await map.getKey(3n);
+        expect(validUntil).toEqual(3000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(345, 16).endCell().asSlice()
+        );
     
-        // blockchain.now = 3001;
+        blockchain.now = 3001;
     
-        // await map.sendClear(deployer.getSender(), toNano('0.05'), {
-        //     queryId: 123n,
-        // });
+        await map.sendClear(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+        });
     
-        // await expect(map.getKey(2n)).rejects.toThrow();
-        // await expect(map.getKey(3n)).rejects.toThrow();
+        await expect(map.getKey(2n)).rejects.toThrow();
+        await expect(map.getKey(3n)).rejects.toThrow();
     });
 });
